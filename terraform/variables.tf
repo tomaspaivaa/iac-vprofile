e5a8296ae6c722a034cbed4a269586347d6211e7
tomaspaivaa <tomaspaivaa7@gmail.com>
2026-03-17 16:50:51 +0000

    sdasdasd

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@ variable "region" {
 }
 
 variable "clusterName" {
-  description = "Name of the EKS clusterrr"
+  description = "Name of the EKS clusterrrrr"
   type        = string
   default     = "vprofile-eks"
 }
